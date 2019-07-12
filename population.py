5c6668a88c279b2c7e84a425ebf559fbf339507a
Leandro Stival <s.tival@hotmail.com>
2019-07-11 23:50:39 -0300

    Ajustes na população

--- population.py
+++ population.py
@@ -1,25 +1,27 @@
 import random
 class Population():
-
+    
     def start_Random_Population(teacher_List, population_List):
-        population_Leng = 10 #Defini o tamanho da populacao
+        population_Leng = 1000 #Defini o tamanho da populacao
         i = 0
 
         while i <= population_Leng:  #Executa a quantida de vezes igual ao tamanho da populacao
             individual = []
+
             for teacher in teacher_List: #Executa uma vez para cada professor dessa solucao
-                avaliable_days = 0
-                for week_Day in teacher.classWeekPeriods: #Executa para cada dia da semana
-                    if avaliable_days <= teacher.classDaysAvaliable: 
-                        if random.choice([True, False]): #Gera um random para adicionar um periodo ao dia da semana
-                            individual.append(random.choice(teacher.periods)) 
-                            avaliable_days += 1
-                        else:
-                            individual.append("0")
-                    else:
-                        individual.append("0")
+                to_Shuffle = ""
+                to_Shuffle = (Population.complet_List_With_Zero(teacher.periods,len(teacher.classWeekPeriods)))
+                random.shuffle(to_Shuffle)
+                individual.extend()
+
             population_List.append(individual)
             i += 1
+
         print("População gerada")
         return(population_List)
 
+    def complet_List_With_Zero(list_To_Complet, leng):
+
+        while len(list_To_Complet) < leng:
+            list_To_Complet.append("0")
+        return (list_To_Complet)
